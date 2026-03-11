0536e05ffc0d379bf16701c8325c0d03fa64963c
ayush-pandey047 <23417dp@gmail.com>
2026-03-11 20:05:13 +0530

    feat: initial project setup

---
+++ src/app.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Assignment Portal API is running' });
+});
+
+module.exports = app;
